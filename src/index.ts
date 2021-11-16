09f6205b7dae9896df301fe8d6277622bbf2dbee
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2021-11-16 15:41:10 +0000

    Remove lib

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-// Export the functions from the data interfaces
-export * from './data/activeDataInterface'
-export * from './data/staticDataInterface'
-
 // Export the model classes
-export * from './models/active/activeModel'
-export * from './models/static/staticModel'
+export { default as ActiveModel } from './models/active/activeModel'
+export { default as StaticModel } from './models/static/staticModel'
 
 // Export the validations
-export * from './validation/validators/staticModelValidator'
-export * from './validation/validators/customValidator'
-export * from './validation/validators/inputValidators/inclusionValidator'
-export * from './validation/validators/inputValidators/numberValidator'
-export * from './validation/validators/inputValidators/stringValidator'
+export { default as StaticModelValidator } from './validation/validators/staticModelValidator'
+export { default as CustomValidator } from './validation/validators/customValidator'
+export { default as InclusionValidator } from './validation/validators/inputValidators/inclusionValidator'
+export { default as NumberValidator } from './validation/validators/inputValidators/numberValidator'
+export { default as StringValidator } from './validation/validators/inputValidators/stringValidator'
