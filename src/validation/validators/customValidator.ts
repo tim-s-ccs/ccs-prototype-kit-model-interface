645f35f95ad4f8b7d048b6326d37cde1306adc69
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2022-02-15 13:21:56 +0000

    Allow custom validator to have generic options

--- src/validation/validators/customValidator.ts
+++ src/validation/validators/customValidator.ts
@@ -1,9 +1,9 @@
 import ActiveModel from '../../models/activeModel'
 import Validator from '../validator'
-import { CustomValidatorInterface, ErrorMessages, ValidatorOptions } from '../../types/validation/validator'
+import { CustomValidatorInterface, ErrorMessages, GenericValidatorOptions } from '../../types/validation/validator'
 
 abstract class CustomValidator extends Validator implements CustomValidatorInterface {
-  constructor(model: ActiveModel, attribute: string, errorMessages: ErrorMessages, options: ValidatorOptions) {
+  constructor(model: ActiveModel, attribute: string, errorMessages: ErrorMessages, options: GenericValidatorOptions) {
     super(model, attribute, errorMessages, options)
   }
 }
